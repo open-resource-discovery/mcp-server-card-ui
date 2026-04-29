d37a6826675ff830a53e3fdb03e8ce0904972789
Anna Alekseeva <anna.alekseeva@sap.com>
2026-04-29 11:10:17 +0200

    Unnecessary comments removed

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./e2e/tests",
   globalSetup: "./e2e/global-setup.ts",
@@ -53,12 +43,6 @@ export default defineConfig({
     },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
   webServer: [
     {
       command: "npm run dev",
